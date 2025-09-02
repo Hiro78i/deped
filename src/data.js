68dd3b3cb855aa9b1393fc6aea82e48336dc6464
Lander <92011230+Hiro78i@users.noreply.github.com>
2025-09-02 16:32:18 +0800

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 export const ACTIONS = [
-    { title: "Deped Email Creation", href: "https://forms.gle/REPLACE_1" },
+    { title: "Deped Email Creation", href: "https://docs.google.com/forms/d/e/1FAIpQLSfqm2QvybEUx9_UTDPZ8Km2nGcF9WkIg2hPYt27uBXiG0dSjw/viewform?usp=header" },
     { title: "Resetting Deped Email Password", href: "https://forms.gle/REPLACE_2" },
     { title: "Resetting DPDS Password", href: "https://forms.gle/REPLACE_3" },
     { title: "Resetting 365 Email Password", href: "https://forms.gle/REPLACE_4" },
