04351eb72f22bf5de14438c8824d251697ed70ea
Lander <92011230+Hiro78i@users.noreply.github.com>
2025-09-03 09:42:01 +0800

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const ACTIONS = [
     { title: "Deped Email Creation", href: "https://docs.google.com/forms/d/e/1FAIpQLSfqm2QvybEUx9_UTDPZ8Km2nGcF9WkIg2hPYt27uBXiG0dSjw/viewform?usp=header" },
-    { title: "Resetting Deped Email Password", href: "https://forms.gle/REPLACE_2" },
-    { title: "Resetting DPDS Password", href: "https://forms.gle/REPLACE_3" },
-    { title: "Resetting 365 Email Password", href: "https://forms.gle/REPLACE_4" },
+    { title: "Resetting Deped Email Password", href: "https://docs.google.com/forms/d/e/1FAIpQLSc34B0eZZ6f7TzUmwYj6OfmGP8N8L1Y-SX50rM-teLP6S5Mzg/viewform" },
+    { title: "Resetting DPDS Password", href: "https://docs.google.com/forms/d/e/1FAIpQLScggCphg9btDNLMLPgoI2YjBLiWXIySXtOECkz0_vvxlMlP1Q/viewform" },
+    { title: "Resetting 365 Email Password", href: "https://docs.google.com/forms/d/e/1FAIpQLSe1ZNtSg-BGZWlMhDdsyTG4O75H13BK0g9Rzg3ucYKmrJ6H-A/viewform?usp=sharing&ouid=112642201672756451061" },
 ];
